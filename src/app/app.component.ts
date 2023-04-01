8f721c3fe9a7a6c49bf04a8cf4dd61fed624cbb7
fhssouza <fhssouza@gmail.com>
2023-04-01 18:08:19 -0300

    ajuste environment, produto service e metodos salvar e criar produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ProdutoService } from './service/produto.service';
+import { Produto } from './model/produto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'front-produto';
+  
+  produtoForm = this.fb.group({
+    id: [],
+    nome: [null, Validators.required],
+    valor: [null, Validators.required],
+    descricao: []
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private produtoService: ProdutoService
+  ){ }
+
+  criarProduto(): Produto{
+    return {
+      id: this.produtoForm.get('id')?.value!,
+      nome: this.produtoForm.get('nome')?.value!,
+      valor: this.produtoForm.get('valor')?.value!,
+      descricao: this.produtoForm.get('descricao')?.value!,
+    }
+  }
+
+  salvar(){
+    if(this.produtoForm.valid){
+     const produto = this.criarProduto();
+     console.log('produto', produto);
+
+     this.produtoService.salvar(produto).subscribe(
+      {
+        next: (res) => {
+          alert("Produto salvo com sucesso!");
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+    }
+  }
 }
